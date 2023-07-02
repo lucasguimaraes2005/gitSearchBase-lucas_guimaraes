55c1afc98b05644894bb8ac575bfa7e84423fbfa
lucas <lucaagui123@gmail.com>
2023-07-02 08:01:33 -0300

    andamento

--- src/scripts/profile.js
+++ src/scripts/profile.js
@@ -1,13 +1,17 @@
-const criarCabecalho = (user) => {
+const criarCabecalho = () => {
+    
+    const user = JSON.parse(localStorage.getItem('gitName'))
+    
+    
     const img = document.querySelector('.image__profile')
-    const name = document.querySelector('.profile__name')
+    const username = document.querySelector('.profile__name')
 
-    name.innerHTML = ''
+    
 
     img.src = user.avatar_url
     img.alt = user.name
 
-    name = user.name
+    username.innerHTML = user.name
 
     const button = document.querySelector('.header__button')
 
@@ -16,3 +20,45 @@ const criarCabecalho = (user) => {
     })
 
 }
+
+const renderizarRepositorios = async () => {
+    
+    const user = JSON.parse(localStorage.getItem('gitName'))
+
+    const repositorios = await fetch(`https://api.github.com/users/${user.name}/repos`, {
+        method: 'GET'
+    })
+    .then( async (response) => {
+        if (response.ok) {
+            const responseConvert = await response.json()
+        } 
+    })
+
+    const mainList = document.querySelector('.profile__ul')
+
+    repositorios.forEach((repositorio) => {
+        const li = document.createElement('li')
+        const h4 = document.createElement('h4')
+        const p = document.createElement('p')
+        const a = document.createElement('a')
+
+        h4.innerText = repositorio.name
+        
+        if (repositorio.description) {
+            p.innerText = repositorio.description
+        } else {
+            p.innerText = "Repositório sem descrição"
+        }
+
+        a.innerText = "Repositório"
+
+        a.href = repositorio.html_url
+        a.target_blank
+
+        li.append(h4, p, a)
+        mainList.append(li)
+    })
+}
+
+renderizarRepositorios()
+criarCabecalho()
